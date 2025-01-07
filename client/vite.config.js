a2b43a9f8650d5c92b18b44b56c3bd2e122ce6eb
i-waseem <iqrawaseem@protonmail.com>
2025-01-07 18:06:59 +0000

    Updated Vite configuration for production build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    commonjsOptions: {
+      include: []
+    }
+  },
+  optimizeDeps: {
+    disabled: false
+  },
+  base: '/'
 })
